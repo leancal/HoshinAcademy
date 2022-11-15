3c4fca2ff8ccb02267c49d940f37085dc6510935
Leandro Calvet <leandrocalvet14@gmail.com>
2022-11-15 11:16:14 -0300

    Details

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -165,7 +165,7 @@ msg = " Academy " + msg;position = 0;
 function scrolltitle() {
 document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
 if (position > msg.length) position = 0
-window.setTimeout("scrolltitle()",170);
+window.setTimeout("scrolltitle()",500);
 }
 scrolltitle();
 

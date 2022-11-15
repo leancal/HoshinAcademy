8b03c4d53ffd99dc976fb9ab8e4f04bad6e7891c
Leandro Calvet <leandrocalvet14@gmail.com>
2022-11-15 09:57:36 -0300

    Details

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -159,5 +159,13 @@ let tiempo=setInterval(() => {
     }
 }, 2);
 
-
+/*==================== COUNTUP ====================*/
+msg = "Hoshin";
+msg = " Academy " + msg;position = 0;
+function scrolltitle() {
+document.title = msg.substring(position, msg.length) + msg.substring(0, position); position++;
+if (position > msg.length) position = 0
+window.setTimeout("scrolltitle()",170);
+}
+scrolltitle();
 
